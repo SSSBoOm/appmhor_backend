fd3261007bbb69742e04a11d4c9e54a63b04ba8e
SSSBoOm <boom1909chanapat@gmail.com>
2023-11-26 12:58:32 +0700

    .

--- src/Routes/user.route.ts
+++ src/Routes/user.route.ts
@@ -3,7 +3,7 @@ const router: Router = express.Router()
 import * as userController from '@/Controller/userTemp.controller'
 
 router.route('/').post(userController.createUser)
-router.route('logout').post(userController.Logout)
+router.route('/logout').post(userController.Logout)
 router.route('/me').get(userController.getUser)
 
 export const userRoute: Router = router
